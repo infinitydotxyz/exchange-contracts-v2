039ab30bf1eb0d6ea8885f162f4dccebf6c32b8e
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-11-01 14:26:36 -0500

    Created: TokenBroker contract. Added: intermediary handling

---
+++ contracts/core/TokenBroker.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import {Ownable} from '@openzeppelin/contracts/access';
+import {Pausable} from '@openzeppelin/contracts/security/Pausable.sol';
+
+/**
+@title TokenBroker
+@author Joe
+@notice The contract that brokers trades among other exchanges
+*/
+contract TokenBroker is Ownable, Pausable {
+  /*//////////////////////////////////////////////////////////////
+                                ADDRESSES
+      //////////////////////////////////////////////////////////////*/
+
+  /// @notice The address of the EOA that acts as an intermediary in the brokerage process
+  address public intermediary;
+
+  /*//////////////////////////////////////////////////////////////
+                                EVENTS
+      //////////////////////////////////////////////////////////////*/
+  event IntermediaryUpdated(address indexed intermediary);
+
+  /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+  constructor(address _intermediary) public {
+    _updateIntermediary(_intermediary);
+  }
+
+  /// @notice Update the intermediary to a different EOA
+  /// @param _intermediary The new intermediary to use
+  function updateIntermediary(address _intermediary) external onlyOwner {
+    _updateIntermediary(_intermediary);
+  }
+
+  function _updateIntermediary(address _intermediary) internal {
+    require(_intermediary != address(0), 'intermdiary cannot be 0');
+    intermediary = _intermediary;
+    emit IntermediaryUpdated(_intermediary);
+  }
+
+  /// @dev Function to pause the contract
+  function pause() external onlyOwner {
+    _pause();
+  }
+
+  /// @dev Function to unpause the contract
+  function unpause() external onlyOwner {
+    _unpause();
+  }
+}
