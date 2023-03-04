3feba997ae0b89c4af64d9568a1aa2014c918473
nneverlander <findadit@gmail.com>
2023-03-04 12:56:04 -0500

    adds goerli owl contracts

---
+++ contracts/nfts/IGowlDescriptor.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+/*********************************
+*                                *
+*               0,0              *
+*                                *
+ *********************************/
+
+pragma solidity 0.8.14;
+
+interface IGowlDescriptor {
+    function tokenURI(uint256 tokenId, uint256 seed) external view returns (string memory);
+}
